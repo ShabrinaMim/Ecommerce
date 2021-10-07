6186e192308e6c09d004dea89b3e9d8cad9fcb46
ShabrinaMim <mimba007@gmail.com>
2021-10-08 05:54:44 +0600

    first commit

--- Server.js
+++ Server.js
@@ -14,7 +14,7 @@ const stripe = require("stripe")(STRIPE_SECRET_KEY);
 const app = express();
 
 //Serve static files from the react app
-app.use(express.static(path.join(__dirname, "build")));
+app.use(express.static(path.join(__dirname, "/build")));
 
 //Middleware
 app.use(cors()); //we are using cors for security reason
@@ -60,7 +60,7 @@ app.post("/payments/create", cors(), async function (req, res) {
 });
 
 app.get("/*", function (req, res) {
-  res.sendFile(path.join(__dirname + "build/index.html"));
+  res.sendFile(path.join(__dirname + "/build/index.html"));
 });
 
 app.listen(PORT, function () {
